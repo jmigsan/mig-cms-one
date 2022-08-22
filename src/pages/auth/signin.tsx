8ead48f496b6a52df189491e54dcad7c42bf4e89
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-08-22 21:40:11 +0100

    sign in things and also prisma

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -2,7 +2,17 @@ import { Box, Button, Center, HStack, Image } from '@chakra-ui/react';
 import { getProviders, signIn } from 'next-auth/react';
 import { useEffect } from 'react';
 
-const SignIn: React.FC<{ providers: any }> = ({ providers }) => {
+const SignIn: React.FC<{
+  providers: {
+    provider: {
+      id: string;
+      name: string;
+      type: string;
+      callbackUrl: string;
+      signinUrl: string;
+    };
+  };
+}> = ({ providers }) => {
   let redirectUrl = 'http://localhost:3000';
 
   useEffect(() => {
@@ -13,12 +23,18 @@ const SignIn: React.FC<{ providers: any }> = ({ providers }) => {
         : 'http://localhost:3000';
   });
 
+  if (providers === undefined) {
+    return null;
+  }
+
+  console.log(providers);
+
   return (
     <>
       <Box>
-        <HStack>
-          <Box>
-            <Center>
+        <Center>
+          <HStack>
+            <Box>
               {Object.values(providers).map((provider: any) => (
                 <Box key={provider.name}>
                   <Button
@@ -30,12 +46,12 @@ const SignIn: React.FC<{ providers: any }> = ({ providers }) => {
                   </Button>
                 </Box>
               ))}
-            </Center>
-          </Box>
-          <Box>
-            <Image src='https://source.unsplash.com/random' />
-          </Box>
-        </HStack>
+            </Box>
+            <Box w='lg'>
+              <Image src='https://source.unsplash.com/random/?nature,water' />
+            </Box>
+          </HStack>
+        </Center>
       </Box>
     </>
   );
