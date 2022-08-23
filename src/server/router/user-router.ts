76a1c304fc1d23fa95f61939fee9c4d68a802998
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-08-23 12:03:41 +0100

    getting more user data

--- src/server/router/user-router.ts
+++ src/server/router/user-router.ts
@@ -1,13 +1,34 @@
 import { createProtectedRouter } from './protected-router';
 
 // Example router with queries that can only be hit if the user requesting is signed in
-export const userRouter = createProtectedRouter().query('getRole', {
-  async resolve({ ctx }) {
-    const userRole = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-      select: { role: true },
-    });
+export const userRouter = createProtectedRouter()
+  .query('getRole', {
+    async resolve({ ctx }) {
+      const userRole = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
+        select: { role: true },
+      });
 
-    return userRole?.role;
-  },
-});
+      return userRole?.role;
+    },
+  })
+  .query('getName', {
+    async resolve({ ctx }) {
+      const userName = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
+        select: { name: true },
+      });
+
+      return userName?.name;
+    },
+  })
+  .query('getImage', {
+    async resolve({ ctx }) {
+      const userImage = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
+        select: { image: true },
+      });
+
+      return userImage?.image;
+    },
+  });
