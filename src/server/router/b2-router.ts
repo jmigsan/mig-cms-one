51525926820029adec87635a595634527431b3ed
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-08-30 17:25:50 +0100

    presigned url authorisation

--- src/server/router/b2-router.ts
+++ src/server/router/b2-router.ts
@@ -2,6 +2,7 @@ import { createProtectedRouter } from './protected-router';
 
 import S3 from 'aws-sdk/clients/s3';
 import AWS from 'aws-sdk';
+import { z } from 'zod';
 
 const ep = new AWS.Endpoint('s3.us-west-004.backblazeb2.com');
 
@@ -14,13 +15,25 @@ const b2 = new S3({
 
 // Example router with queries that can only be hit if the user requesting is signed in
 export const b2Router = createProtectedRouter().query('uploadImage', {
-  async resolve({ ctx }) {
-    const signedUrl = b2.getSignedUrl('getObject', {
-      Key: key,
-      Bucket: bucket,
-      Expires: expires || 900, // S3 default is 900 seconds (15 minutes)
-    });
+  input: z.object({
+    key: z.string(),
+  }),
+  async resolve({ ctx, input }) {
+    if (ctx.session.user.id) {
+      const userRole = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
+        select: { role: true },
+      });
 
-    return signedUrl;
+      if (userRole?.role === 'ADMIN') {
+        const signedUrl = b2.getSignedUrl('getObject', {
+          Key: input.key,
+          Bucket: 'mig-cms-one',
+          Expires: 900, // S3 default is 900 seconds (15 minutes)
+        });
+
+        return signedUrl;
+      }
+    }
   },
 });
