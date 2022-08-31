d5744f23ee41cb379bb9bb691b7336a9961f7854
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-08-31 18:34:54 +0100

    i hear guard clauses are cool

--- src/server/router/b2-router.ts
+++ src/server/router/b2-router.ts
@@ -7,40 +7,44 @@ import { b2 } from '../libs/b2';
 export const b2Router = createProtectedRouter()
   .mutation('createPresignedPost', {
     async resolve({ ctx }) {
-      if (ctx.session.user.id) {
-        const userRole = await ctx.prisma.user.findFirst({
-          where: { id: ctx.session.user.id },
-          select: { role: true },
-        });
-
-        if (userRole?.role === 'ADMIN') {
-          const fileId = nanoid();
-
-          // const signedUrl = b2.getSignedUrl('putObject', {
-          //   Bucket: 'mig-cms-one',
-          //   Key: fileId,
-          //   Expires: 300,
-          // });
-
-          const signedUrl = b2.createPresignedPost({
-            Bucket: 'mig-cms-one',
-            Fields: {
-              key: fileId,
-            },
-            Conditions: [
-              ['content-length-range', 0, 67108864], // content length restrictions: 0-64MB
-              ['starts-with', '$Content-Type', 'image/'], // content type restriction
-              ['eq', '$x-amz-meta-userid', ctx.session.user.id], // tag with userid <- the user can see this!
-            ],
-          });
-
-          signedUrl.fields['x-amz-meta-userid'] = ctx.session.user.id; // Don't forget to add this field too
-
-          console.log(fileId, signedUrl);
-
-          return { filename: fileId, presignedurl: signedUrl };
-        }
+      if (!ctx.session.user.id) {
+        throw new Error('please sign in');
       }
+
+      const userRole = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
+        select: { role: true },
+      });
+
+      if (userRole?.role !== 'ADMIN') {
+        throw new Error('you are not authorised');
+      }
+
+      const fileId = nanoid();
+
+      // const signedUrl = b2.getSignedUrl('putObject', {
+      //   Bucket: 'mig-cms-one',
+      //   Key: fileId,
+      //   Expires: 300,
+      // });
+
+      const signedUrl = b2.createPresignedPost({
+        Bucket: 'mig-cms-one',
+        Fields: {
+          key: fileId,
+        },
+        Conditions: [
+          ['content-length-range', 0, 67108864], // content length restrictions: 0-64MB
+          ['starts-with', '$Content-Type', 'image/'], // content type restriction
+          ['eq', '$x-amz-meta-userid', ctx.session.user.id], // tag with userid <- the user can see this!
+        ],
+      });
+
+      signedUrl.fields['x-amz-meta-userid'] = ctx.session.user.id; // Don't forget to add this field too
+
+      console.log(fileId, signedUrl);
+
+      return { filename: fileId, presignedurl: signedUrl };
     },
   })
   .mutation('uploadToB2', {
